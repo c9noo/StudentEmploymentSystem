11dd0c9d0badd9483d28b7773f62546d6310e439
c9noo <c9noo@aliyun.com>
2023-12-29 19:46:30 +0800

    修正:删除多余代码

---
+++ StudentEmploymentRearEnd/admin/src/main/java/com/employment/controller/StudentController.java
@@ -0,0 +1,18 @@
+package com.employment.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: StudentEmploymentSystem
+ * @ClassName StudentController
+ * @author: c9noo
+ * @create: 2023-12-29 19:33
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/admin/student")
+@Api("学生管理")
+public class StudentController {
+}
--- StudentEmploymentRearEnd/admin/src/main/java/com/employment/task/ClassTask.java
+++ StudentEmploymentRearEnd/admin/src/main/java/com/employment/task/ClassTask.java
@@ -4,7 +4,6 @@ import com.employment.constant.ClassGraduateStatusConstant;
 import com.employment.mapper.ClassInfoMapper;
 import com.employment.pojo.dto.ClassIdAndTime;
 import com.employment.pojo.entity.ClassInformation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
